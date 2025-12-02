3128c5cc612b01ea97991d3ed439c579007fca7c
JoelAlgera <Joel.Algera@student.ufv.ca>
2025-12-01 19:03:52 -0800

    Polished Paddle Physics
    Another commit has hit the repo!
    
    And its a doozy

--- assignment-3/Pinball/pinballphysics.gd
+++ assignment-3/Pinball/pinballphysics.gd
@@ -1,12 +1,17 @@
 extends Node3D
 var fire_in_the_hole = false
+var can_bump = false
 
 @onready var _ball: RigidBody3D = $Ball
-@onready var _bumper1: RigidBody3D = $Bumper1
+@onready var _bumper1: Area3D = $Bumper1/LeftBumpArea
 
 func _ready():
 	$MachineFace/FiringHole.body_entered.connect(_on_body_entered)
 	$MachineFace/FiringHole.body_exited.connect(_on_body_exited)
+	$Bumper1/LeftBumpArea.body_entered.connect(_on_bumper_entered)
+	$Bumper1/LeftBumpArea.body_exited.connect(_on_bumper_exited)
+	$Bumper2/RightBumpArea.body_entered.connect(_on_bumper_entered)
+	$Bumper2/RightBumpArea.body_exited.connect(_on_bumper_exited)
 
 
 func _on_body_entered(body):
@@ -18,26 +23,33 @@ func _on_body_exited(body):
 	if body.name == "Ball":
 		fire_in_the_hole = false
 
-func _on_bumper_body_entered(body):
+func _on_bumper_entered(body):
 	if body.name == "Ball":
-		pass
+		print("LEFT")
+		can_bump = true
+		
+func _on_bumper_exited(body):
+	if body.name == "Ball":
+		can_bump = false
+		
+
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("jump"):
 		if fire_in_the_hole:
 			print("FIRING")
 		else:  
-			print("NOPE")
-
+			pass
 func _physics_process(delta: float) -> void:
-	if Input.is_action_just_pressed("jump") and fire_in_the_hole == true:
+	if Input.is_action_just_pressed("jump") and fire_in_the_hole:
 		var direction := -transform.basis.z.normalized()
 		var strength := 10
 		_ball.apply_impulse(direction * strength)
-		pass
-	if Input.is_action_just_pressed("ui_left") and _on_bumper_body_entered(_bumper1):
-		var direction := transform.basis.z.normalized()
-		var strength := 100
+
+	if (Input.is_action_just_pressed("ui_left") or Input.is_action_just_pressed("ui_right")) and can_bump:
+		print("BUMPIN")
+		var direction := -transform.basis.z.normalized()
+		var strength := 5
 		_ball.apply_impulse(direction * strength)
 		pass
 		
